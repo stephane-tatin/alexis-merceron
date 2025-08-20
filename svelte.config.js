0e19e1328533948a2251c5e92195c4ad6089504f
Stephane Tatin <stephane.tatin@westernacher.com>
2025-08-20 14:01:00 +0200

    adjust

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -19,7 +20,7 @@ const config = {
       fallback: 'index.html'
 		}),
 		paths: {
-			base: process.env.BASE_PATH || ''
+			base: dev ? '' : '/alexis-merceron' 
 		}
 	}
 };
