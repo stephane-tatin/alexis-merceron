8c213734e0def94e924460503677f998b52193c6
Stéphane Tatin <stephane.tatin@gmail.com>
2025-08-24 08:36:49 +0200

    Adjust

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -56,7 +56,7 @@
 		{/each}
 	</div>
 	<div class="w-full max-w-xl mt-10 px-4">
-		<h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Liens:</h2>
+		<p class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Liens:</p>
 		<ul class="list-disc list-inside text-gray-500 dark:text-gray-400 space-y-1">
 			{#each data.links as link}
 				<div class="link-container">
